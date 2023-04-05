c8a1e96f66658ec7b7f3ec8d96e86e3b81b4afb5
jinkim31 <87002847+jinkim31@users.noreply.github.com>
2023-04-05 15:31:22 +0900

    Update noetic.sh

--- noetic.sh
+++ noetic.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #sudo chown -R jinkim ~/miniconda3
-conda install mamba -c conda-forge
-mamba create -n ros ros-noetic-desktop-full -c conda-forge -c robostack-staging
+yes | conda install mamba -c conda-forge
+yes | mamba create -n ros ros-noetic-desktop-full -c conda-forge -c robostack-staging
 conda activate ros
-mamba install compilers cmake pkg-config make ninja colcon-common-extensions
-mamba install catkin_tools
+yes | mamba install compilers cmake pkg-config make ninja colcon-common-extensions
+yes | mamba install catkin_tools
 conda deactivate
 conda activate ros
-mamba install rosdep
+yes | mamba install rosdep
 rosdep init
 rosdep update
 conda deactivate
