c1ce46bb5c1a210bd637be3cb9c0d87922c87a94
jinkim31 <87002847+jinkim31@users.noreply.github.com>
2023-04-05 17:24:13 +0900

    Update noetic.sh

--- noetic.sh
+++ noetic.sh
@@ -3,6 +3,9 @@
 yes | conda install mamba -c conda-forge
 yes | mamba create -n ros ros-noetic-desktop-full -c conda-forge -c robostack-staging
 conda activate ros
+conda config --env --add channels conda-forge
+conda config --env --add channels robostack-staging
+conda config --env --remove channels defaults
 yes | mamba install compilers cmake pkg-config make ninja colcon-common-extensions
 yes | mamba install catkin_tools
 conda deactivate
